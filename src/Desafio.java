24f3d70f0fc87b3e5f76cc06c0176648bc0b6979
LaryssaGabi <lary.gabriela85@gmail.com>
2025-05-15 22:57:10 -0300

    menu de info

--- src/Desafio.java
+++ src/Desafio.java
@@ -7,11 +7,26 @@ public class Desafio {
         String nome = "Laryssa Gabriela";
         String tipoConta = "Corrente";
         double saldo = 1900.00;
+        int opcao = 0;
+
 
         System.out.println("-----Dados Bancarios-----");
         System.out.println("\nNome do cliente: " + nome);
         System.out.println("Tipo de conta: " + tipoConta);
         System.out.println("Saldo atual: " + saldo);
         System.out.println("\n**************************");
+
+        String menu = """
+                *** Digite sua opção ***
+                1 - Consultar Saldo
+                2 - Tranferir valor
+                3 - Receber valor
+                4 - Sair
+                """;
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leia.nextInt();
+        }
     }
 }
